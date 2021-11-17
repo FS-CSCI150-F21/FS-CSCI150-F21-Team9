7ac27a3d092cda70c78c0c7fd07e3f04eb6189ed
marcos1170 <marcos1170@mail.fresnostate.edu>
2021-11-16 17:01:14 -0800

    updated links
    Calendar and Taskboard

--- y/index.js
+++ y/index.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // Initialize variables
 const auth = firebase.auth();
-const database = firebase.realtime();
+const database = firebase.firestore();
 
 //listen for auth status change 
 auth.onAuthStateChanged(user => {
