7741700677348cac9def72ec049dc4a59f035476
Julie <jmendez1234@mail.fresnostate.edu>
2021-11-25 08:04:07 -0800

    Rearranged add task and task list + add category, need to make add task realtime

--- y/taskBoard/julie_task.js
+++ y/taskBoard/julie_task.js
@@ -6,15 +6,18 @@ function renderTasklist(doc){
     let li = document.createElement('li');
     let task = document.createElement('span');
     let description = document.createElement('span');
+    let category = document.createElement('span');
     let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id)
     task.textContent = doc.data().task;
     description.textContent = doc.data().description;
+    category.textContent = doc.data().category;
     cross.textContent = 'x';
 
     li.appendChild(task);
     li.appendChild(description);
+    li.appendChild(category);
     li.appendChild(cross);
 
     tasklist.appendChild(li);
@@ -40,14 +43,16 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     database.collection('tasks').add({
         task: form.task.value,
-        description: form.description.value
+        description: form.description.value,
+        category: form.category.value
     });
 form.task.value = '';
 form.description.value ='';
+form.category.value ='';
 })
 
 // realtime listener
-database.collection('tasks').orderBy('task').onSnapshot(snapshot => {
+database.collection('tasks').orderBy('category').onSnapshot(snapshot => { // changed task to category
     let changes = snapshot.docChanges();
     // console.log(changes);
     changes.forEach(change => {
